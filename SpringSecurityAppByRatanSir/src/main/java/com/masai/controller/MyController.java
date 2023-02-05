2da724dfc875ddc1a9a3fd67a386d48fea5c317e
Harshit Bansal <harshitbansal394@gmail.com>
2023-02-05 20:56:44 +0530

    add Spring Security Notes

---
+++ SpringSecurityAppByRatanSir/src/main/java/com/masai/controller/MyController.java
@@ -0,0 +1,18 @@
+package com.masai.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MyController {
+
+	@GetMapping("/hello")
+	public String sayHello() {
+		return "Welcome to the Spring Security Method 1";
+	}
+	
+	@GetMapping("/hello2")
+	public String sayHello2() {
+		return "Welcome to the Spring Security Method 2";
+	}
+}
